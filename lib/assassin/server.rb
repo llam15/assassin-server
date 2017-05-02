0805d0cb564df217179c9c0ce277210237587158
Ky-Cuong Huynh <kycodehuynh@gmail.com>
2017-05-02 14:57:48 -0700

    Create POST '/game/create' route

--- lib/assassin/server.rb
+++ lib/assassin/server.rb
@@ -9,11 +9,16 @@
 require "sinatra/base"
 require "sinatra/activerecord"
 
+# TODO: Move models into separate files
 class Player < ActiveRecord::Base
 end
 
 class Game < ActiveRecord::Base
   has_many :players
+
+  def players
+    @players = Player.all.select { |player| player.game_id == self.id }
+  end
 end
 
 module Assassin
@@ -28,8 +33,22 @@ module Assassin
       'Hello world'
     end
 
+    # TODO: specify a game ID
     get '/game/players' do
       Player.all.to_json
+    end
+
+    # Receives { username: <username> }
+    post '/game/create' do
+      parsed_request_body = JSON.parse (request.body.read)
+      username = parsed_request_body['username']
+
+      # While we have Game ID's, only have 1 game globally for now
+      unless Game.first.nil?
+        'A global game already exists'
+      else
+        @game = Game.create(status: 'SettingUp')
+      end
     end
 
     # Allow direct execution of the app via 'ruby server.rb'
