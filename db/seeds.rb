aba2a2a122a9b2b4e5ee6e8312e5e80684a3f847
binerys <bree.nery@gmail.com>
2017-05-29 22:28:42 -0700

    Update seeds file to populate database as if game is in-progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,27 @@
-Game.create({status: 'SettingUp'})
+Game.create({status: 'InProgress'})
 
 gm = Player.new(username: 'gm', role: 'GameMaster', alive: true)
 gm.game = Game.first
 gm.save
 
-participant_1 = Player.new(username: 'player_1', role: 'Participant', alive: true)
+participant_1 = Player.new(username: 'kycuong', role: 'Participant', alive: true)
 participant_1.game = Game.first
 participant_1.save
 
-participant_2 = Player.new(username: 'player_2', role: 'Participant', alive: true)
+participant_2 = Player.new(username: 'bnery', role: 'Participant', alive: true)
 participant_2.game = Game.first
 participant_2.save
 
-participant_3 = Player.new(username: 'player_3', role: 'Participant', alive: true)
+participant_3 = Player.new(username: 'meeshic', role: 'Participant', alive: true)
 participant_3.game = Game.first
 participant_3.save
 
 TargetAssignment.add_new_assignment(1,2)
 TargetAssignment.add_new_assignment(2,3)
-TargetAssignment.add_new_assignment(3,1)
+TargetAssignment.add_new_assignment(3,4)
+TargetAssignment.add_new_assignment(4,1)
+
+Player.find_by(username: "gm").update(latitude: 44.981677, longitude:  -93.27833)
+Player.find_by(username: "kycuong").update(latitude: 44.981867, longitude:  -93.27833)
+Player.find_by(username: "bnery").update(latitude: 44.981669, longitude:  -93.27833)
+Player.find_by(username: "meeshic").update(latitude: 44.981672, longitude:  -93.27833)
